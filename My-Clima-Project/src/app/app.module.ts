0c895731bb7f5f305209f7de4338500cf11765b4
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-03-06 17:24:40 +0200

    Add main component

--- My-Clima-Project/src/app/app.module.ts
+++ My-Clima-Project/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
