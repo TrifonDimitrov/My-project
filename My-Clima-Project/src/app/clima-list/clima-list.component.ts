2e66586857d3f7f6eac3f3fed088eaf00d4349c6
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-03-13 15:49:58 +0200

    Sample data recall from the server

--- My-Clima-Project/src/app/clima-list/clima-list.component.ts
+++ My-Clima-Project/src/app/clima-list/clima-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-clima-list',
   templateUrl: './clima-list.component.html',
   styleUrls: ['./clima-list.component.css']
 })
-export class ClimaListComponent {
+export class ClimaListComponent implements OnInit {
+  constructor(private apiService: ApiService) { }
 
+  ngOnInit(): void{
+    this.apiService.getModels().subscribe((data: any) => {
+      console.log(data);
+    });
+  }
 }
