b7ee663e92bd1b78b5b7f1becf37f86f14f5ce58
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-03-18 18:44:00 +0200

    Update components

--- My-Clima-Project/src/app/user/user.module.ts
+++ My-Clima-Project/src/app/user/user.module.ts
@@ -4,18 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
+  imports: [CommonModule, UserRoutingModule, RouterModule],
 })
-export class UserModule { }
+export class UserModule {}
