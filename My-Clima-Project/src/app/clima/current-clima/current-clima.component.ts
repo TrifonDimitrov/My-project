211439174a71a75df5c5349f9e2969e762cabf59
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-03-24 17:24:32 +0200

    Update current-clima.component.ts

--- My-Clima-Project/src/app/clima/current-clima/current-clima.component.ts
+++ My-Clima-Project/src/app/clima/current-clima/current-clima.component.ts
@@ -4,9 +4,9 @@ import { ApiService } from 'src/app/api.service';
 import { Climate } from 'src/app/types/climate.model';
 
 @Component({
-  selector: 'app-current-clima',
-  templateUrl: './current-clima.component.html',
-  styleUrls: ['./current-clima.component.css'],
+  selector: 'app-current-clima', 
+  templateUrl: './current-clima.component.html', 
+  styleUrls: ['./current-clima.component.css'], 
 })
 export class CurrentClimaComponent implements OnInit {
   clima = {} as Climate; // Тук ще се пази климатика, която ще се показва в шаблона
@@ -15,10 +15,10 @@ export class CurrentClimaComponent implements OnInit {
 
   ngOnInit(): void {
     this.activeRoute.params.subscribe((params) => {
-      const modelId = params['modelId'];
+      const modelId = params['modelId']; // Взимаме id-то на климатиката от URL-а
 
-      this.api.getClima(modelId).subscribe((clima) => {
-        this.clima = clima;
+      this.api.getClima(modelId).subscribe((clima) => { // Извикваме метода от ApiService, който връща климатика по id
+        this.clima = clima; 
       });
     });
   }
