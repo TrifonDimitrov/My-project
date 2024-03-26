8f0de9c7ccb7d2ec1427381cc0fd53e44d449e52
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-03-26 18:55:42 +0200

    Update clima module

--- My-Clima-Project/src/app/app.module.ts
+++ My-Clima-Project/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -9,7 +8,6 @@ import { PostsListComponent } from './posts-list/posts-list.component';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { UserModule } from './user/user.module';
-import { AddClimaComponent } from './clima/add-clima/add-clima.component';
 import { ClimaModule } from './clima/clima.module';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
@@ -17,7 +15,7 @@ import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
-  declarations: [AppComponent, PostsListComponent, AddClimaComponent, HomeComponent, ErrorComponent],
+  declarations: [AppComponent, PostsListComponent, HomeComponent, ErrorComponent],
   imports: [BrowserModule, CoreModule, SharedModule, HttpClientModule, FormsModule, UserModule, ClimaModule, AppRoutingModule ],
   providers: [],
   bootstrap: [AppComponent],
--- My-Clima-Project/src/app/clima/clima.module.ts
+++ My-Clima-Project/src/app/clima/clima.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurrentClimaComponent } from './current-clima/current-clima.component';
 import { ClimaRoutingModule } from './clima-routing.module';
-
 import { MainComponent } from './main/main.component';
 import { ClimaListComponent } from './clima-list/clima-list.component';
+import { FormsModule } from '@angular/forms';
+import { AddClimaComponent } from './add-clima/add-clima.component';
 
 
 @NgModule({
-  declarations: [CurrentClimaComponent, ClimaListComponent, MainComponent],
-  imports: [CommonModule, ClimaRoutingModule],
+  declarations: [CurrentClimaComponent, ClimaListComponent, MainComponent, AddClimaComponent],
+  imports: [CommonModule, ClimaRoutingModule, FormsModule],
 })
 export class ClimaModule {}
